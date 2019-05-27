88c630c8cab63c716f8d0bd7831366c5944d3cd3
lgjlife <liang_gj17@163.com>
2019-05-27 11:35:18 +0800

    update .gitignore

--- Java-base/target/generated-sources/annotations/com/code/base/string/generated/JmhString_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.code.base.string.generated;
-public class JmhString_jmhType extends JmhString_jmhType_B3 {
-}
-
--- Java-base/target/generated-sources/annotations/com/code/jmh/generated/JmhDemo_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.code.jmh.generated;
-public class JmhDemo_jmhType extends JmhDemo_jmhType_B3 {
-}
-
